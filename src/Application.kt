8a92a84d61ad96094e8b1e16087f84912a2c1ba7
misaosyushi <misaki.tsujita@rakus-partners.co.jp>
2020-05-02 13:27:37 +0900

    autoreload設定追加

--- src/Application.kt
+++ src/Application.kt
@@ -1,23 +1,24 @@
 package com.annime.batch
 
 import io.ktor.application.*
-import io.ktor.http.ContentType
-import io.ktor.http.HttpStatusCode
 import io.ktor.response.*
-import io.ktor.request.*
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
 fun main(args: Array<String>) {
-    val server = embeddedServer(Netty, 8081) {
-        routing {
-            get("/") {
-                call.respond(HttpStatusCode.OK,"Hello, Kotlin")
-            }
+    embeddedServer(
+        Netty, watchPaths = listOf("build/classes"), port = 8081,
+        module = Application::mymodule
+    ).start(true)
+}
+
+fun Application.mymodule() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
         }
     }
-    server.start()
 }
 
