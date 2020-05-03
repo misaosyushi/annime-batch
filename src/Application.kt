00ee0910ef05e6c903cc44d210204331839cad36
misaosyushi <misaki.tsujita@rakus-partners.co.jp>
2020-05-03 19:06:20 +0900

    リファクタ

--- src/Application.kt
+++ src/Application.kt
@@ -1,73 +1,16 @@
 package com.annime.batch
 
-import com.annime.batch.controller.sampleController
-import com.annime.batch.domain.annime.Casts
-import com.annime.batch.domain.annime.Episodes
-import com.annime.batch.domain.annime.Work
-import com.annime.batch.usecase.AnnimeServiceImpl
-import com.annime.batch.usecase.CastServiceImple
-import com.annime.batch.usecase.EpisodeServiceImpl
-import com.annime.batch.usecase.SeasonServiceImpl
-import com.google.gson.Gson
+import com.annime.batch.infrastructure.httpclient.AnnictClient
 import io.ktor.application.*
-import io.ktor.features.ContentNegotiation
-import io.ktor.gson.gson
-import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import org.jetbrains.exposed.sql.Database
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.apache.Apache
-import io.ktor.client.request.*
 
 suspend fun main(args: Array<String>) {
     Database.connect("jdbc:mysql://0.0.0.0:13306/annime?useSSL=false", "com.mysql.jdbc.Driver", "annime", "annime")
 
-    val client = HttpClient(Apache)
-
-    val schemeName = "https"
-    val hostName = "api.annict.com"
-    val portNumber = 443
-    val accessToken = System.getenv("ACCESS_TOKEN")
-    val season = System.getenv("SEASON")
-
-    val works = client.get<String>(
-        scheme = schemeName,
-        host = hostName,
-        port = portNumber,
-        path = "/v1/works?access_token=$accessToken&filter_season=$season&sort_watchers_count=desc"
-    )
-
-    val parsedWorks = Gson().fromJson(works, Work::class.java)
-    insertData(parsedWorks, season)
-
-//    parsedWorks.works.map {
-//        val workId = it.id
-//        val episodes = client.get<String>(
-//            scheme = schemeName,
-//            host = hostName,
-//            port = portNumber,
-//            path = "/v1/episodes?access_token=$accessToken&filter_work_id=$workId&sort_sort_number=asc"
-//        )
-//        insertEpisodes(Gson().fromJson(episodes, Episodes::class.java), workId)
-//        // NOTE: 429エラー回避のため2秒待機させてる
-//        Thread.sleep(2000)
-//    }
-
-    parsedWorks.works.map {
-        val workId = it.id
-        val episodes = client.get<String>(
-            scheme = schemeName,
-            host = hostName,
-            port = portNumber,
-            path = "/v1/casts?access_token=$accessToken&filter_work_id=$workId&sort_sort_number=asc"
-        )
-        insertCasts(Gson().fromJson(episodes, Casts::class.java), workId)
-        // NOTE: 429エラー回避のため2秒待機させてる
-        Thread.sleep(2000)
-    }
-
-    client.close()
+    val client = AnnictClient()
+    client.requestAnnictAPI()
 
     embeddedServer(
         Netty, watchPaths = listOf("build/classes"), port = 8081,
@@ -76,30 +19,5 @@ suspend fun main(args: Array<String>) {
 }
 
 fun Application.mymodule() {
-    install(ContentNegotiation) {
-        gson()
-    }
-    routing {
-        sampleController()
-    }
-}
-
-fun insertData(annimes: Work, season: String) {
-    val seasonService = SeasonServiceImpl()
-    val annimeService = AnnimeServiceImpl()
-
-    val seasonId = seasonService.updateOrInsert(seasonService.findBySeasonText(season), season).id
-
-    annimeService.updateOrInsert(annimes.works, seasonId.value)
-}
-
-fun insertEpisodes(episodes: Episodes, workId: Long) {
-    val episodeService = EpisodeServiceImpl()
-    episodeService.updateOrInsert(episodes.episodes, workId)
-}
-
-fun insertCasts(casts: Casts, workId: Long) {
-    val castService = CastServiceImple()
-    castService.updateOrInsert(casts.casts, workId)
 }
 
---
+++ src/infrastructure/httpclient/AnnictClient.kt
@@ -0,0 +1,83 @@
+package com.annime.batch.infrastructure.httpclient
+
+import com.annime.batch.domain.annime.Casts
+import com.annime.batch.domain.annime.Episodes
+import com.annime.batch.domain.annime.Work
+import com.annime.batch.usecase.AnnimeServiceImpl
+import com.annime.batch.usecase.CastServiceImple
+import com.annime.batch.usecase.EpisodeServiceImpl
+import com.annime.batch.usecase.SeasonServiceImpl
+import com.google.gson.Gson
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.apache.Apache
+import io.ktor.client.request.get
+
+class AnnictClient {
+
+    suspend fun requestAnnictAPI() {
+        val client = HttpClient(Apache)
+
+        val schemeName = "https"
+        val hostName = "api.annict.com"
+        val portNumber = 443
+        val accessToken = System.getenv("ACCESS_TOKEN")
+        val season = System.getenv("SEASON")
+
+        val works = client.get<String>(
+            scheme = schemeName,
+            host = hostName,
+            port = portNumber,
+            path = "/v1/works?access_token=$accessToken&filter_season=$season&sort_watchers_count=desc"
+        )
+
+        val parsedWorks = Gson().fromJson(works, Work::class.java)
+        saveAnnimes(parsedWorks, season)
+
+        parsedWorks.works.map {
+            val workId = it.id
+            val episodes = client.get<String>(
+                scheme = schemeName,
+                host = hostName,
+                port = portNumber,
+                path = "/v1/episodes?access_token=$accessToken&filter_work_id=$workId&sort_sort_number=asc"
+            )
+            saveEpisodes(Gson().fromJson(episodes, Episodes::class.java), workId)
+            // NOTE: 429エラー回避のため2秒待機させてる
+            Thread.sleep(2000)
+        }
+
+        parsedWorks.works.map {
+            val workId = it.id
+            val episodes = client.get<String>(
+                scheme = schemeName,
+                host = hostName,
+                port = portNumber,
+                path = "/v1/casts?access_token=$accessToken&filter_work_id=$workId&sort_sort_number=asc"
+            )
+            saveCasts(Gson().fromJson(episodes, Casts::class.java), workId)
+            // NOTE: 429エラー回避のため2秒待機させてる
+            Thread.sleep(2000)
+        }
+
+        client.close()
+    }
+
+    fun saveAnnimes(annimes: Work, season: String) {
+        val seasonService = SeasonServiceImpl()
+        val annimeService = AnnimeServiceImpl()
+
+        val seasonId = seasonService.updateOrInsert(seasonService.findBySeasonText(season), season).id
+
+        annimeService.updateOrInsert(annimes.works, seasonId.value)
+    }
+
+    fun saveEpisodes(episodes: Episodes, workId: Long) {
+        val episodeService = EpisodeServiceImpl()
+        episodeService.updateOrInsert(episodes.episodes, workId)
+    }
+
+    fun saveCasts(casts: Casts, workId: Long) {
+        val castService = CastServiceImple()
+        castService.updateOrInsert(casts.casts, workId)
+    }
+}
