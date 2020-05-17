82297909e16e624f713d7036055ad7a121b7d495
misaosyushi <misaki.tsujita@rakus-partners.co.jp>
2020-05-17 17:14:16 +0900

    lambdaで動かすための設定追加

--- src/Application.kt
+++ src/Application.kt
@@ -1,12 +1,23 @@
 package com.annime.batch
 
+import com.amazonaws.services.lambda.runtime.Context
 import com.annime.batch.infrastructure.httpclient.AnnictClient
 import org.jetbrains.exposed.sql.Database
 
-suspend fun main(args: Array<String>) {
+suspend fun main() {
     Database.connect("jdbc:mysql://0.0.0.0:13306/annime?useSSL=false", "com.mysql.jdbc.Driver", "annime", "annime")
 
     val client = AnnictClient()
     client.requestAnnictAPI()
+}
+
+class LambdaEntry {
+    // lambda用のエントリーポイント
+    suspend fun runMainAtLambda(context: Context) {
+        val lambdaLogger = context.logger
+        lambdaLogger.log("=========== start ===========")
+        main()
+        lambdaLogger.log("=========== finish ===========")
+    }
 }
 
