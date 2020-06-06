a15d27cf03084e486157193c248745a2adc86fdf
misaosyushi <misaki.tsujita@rakus-partners.co.jp>
2020-06-06 17:46:03 +0900

    DBの情報を設定ファイルに外だし

--- src/Application.kt
+++ src/Application.kt
@@ -2,22 +2,40 @@ package com.annime.batch
 
 import com.amazonaws.services.lambda.runtime.Context
 import com.annime.batch.infrastructure.httpclient.AnnictClient
+import com.typesafe.config.ConfigFactory
+import io.ktor.config.HoconApplicationConfig
+import kotlinx.coroutines.runBlocking
 import org.jetbrains.exposed.sql.Database
 
-suspend fun main() {
-    Database.connect("jdbc:mysql://0.0.0.0:13306/annime?useSSL=false", "com.mysql.jdbc.Driver", "annime", "annime")
+// ローカルで動かしたいときはこっちを使う
+//suspend fun main() {
+//    initDB()
+//    val client = AnnictClient()
+//    client.requestAnnictAPI()
+//}
 
-    val client = AnnictClient()
-    client.requestAnnictAPI()
+// Lambdaのエントリーポイント
+fun runMainAtLambda(context: Context) = runBlocking {
+    initDB()
+    startAnimeBatch(context)
 }
 
-class LambdaEntry {
-    // lambda用のエントリーポイント
-    suspend fun runMainAtLambda(context: Context) {
-        val lambdaLogger = context.logger
-        lambdaLogger.log("=========== start ===========")
-        main()
-        lambdaLogger.log("=========== finish ===========")
-    }
+suspend fun startAnimeBatch(context: Context) {
+    val lambdaLogger = context.logger
+    lambdaLogger.log("=========== start ===========")
+    val client = AnnictClient()
+    client.requestAnnictAPI()
+    lambdaLogger.log("=========== finish ===========")
+
+}
+
+fun initDB() {
+    val config = HoconApplicationConfig(ConfigFactory.load())
+    Database.connect(
+        config.property("db.url").getString(),
+        config.property("db.driver").getString(),
+        config.property("db.user").getString(),
+        config.property("db.pass").getString()
+    )
 }
 
